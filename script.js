266a6ea971fffbb73a887d4703a39c6e350bb0c6
adamkaiserov <46087846+levon-dalakyan@users.noreply.github.com>
2022-10-29 19:45:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const score0El = document.querySelector('.score--0');
+const score1El = document.querySelector('.score--1');
+const current0El = document.querySelector('.current--0');
+const current1El = document.querySelector('.current--1');
+const dice = document.querySelector('.dice');
