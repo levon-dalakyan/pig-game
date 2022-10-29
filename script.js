4caa822185fe9da66fe97697c3e043739dc72c60
adamkaiserov <46087846+levon-dalakyan@users.noreply.github.com>
2022-10-29 20:42:40 +0300

    Add dice image changing; Improve current score when player changing

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 'use strict';
 
-const score0El = document.querySelector('.score--0');
-const score1El = document.querySelector('.score--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
 const current1El = document.querySelector('#current--1');
-const dice = document.querySelector('.dice');
+const player0El = document.querySelector(`.player--0`);
+const player1El = document.querySelector(`.player--1`);
 
-const btnRoll = document.querySelector('.btn--roll');
+const diceEl = document.querySelector('.dice');
+const btnRollEl = document.querySelector('.btn--roll');
 
 let activePlayer = 0;
 let current = 0;
 let scores = [0, 0];
 
-btnRoll.addEventListener('click', function () {
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+btnRollEl.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
 
-  if (dice === 1) {
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    current = 0;
+  diceEl.src = `images/dice-${dice}.png`;
 
-    document.querySelector(`.player--0`).classList.toggle('player--active');
-    document.querySelector(`.player--1`).classList.toggle('player--active');
+  if (dice === 1) {
+    // scores[activePlayer] = +current;
+    // document.querySelector(`#score--${activePlayer}`).textContent =
+    //   scores[activePlayer];
+
+    current = 0;
+    document.querySelector(`#current--${activePlayer}`).textContent = current;
+
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   } else {
     current += dice;
 
